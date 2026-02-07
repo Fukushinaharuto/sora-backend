f01e0b3acc2ff11452cf28cb0ad589a22cce49ad
tomoto-png <hirosi.tomoto0130@gmail.com>
2026-02-08 00:10:07 +0900

    緊急時の助け処理の追加

--- sora-back/routes/api.php
+++ sora-back/routes/api.php
@@ -16,4 +16,7 @@ Route::middleware("auth:sanctum")->group(function () {
     Route::post('/post', [PostController::class, 'store']);
     Route::post('/post/like', [PostController::class, 'like']);
     Route::get('/help', [HelpController::class, 'index']);
+    Route::post('/help', [HelpController::class, 'store']);
+    Route::post('/helped', [HelpController::class, 'markHelped']);
+    Route::post('/help/assignments', [HelpController::class, 'assign']);
 });
